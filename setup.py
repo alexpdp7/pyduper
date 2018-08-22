3c1a390d787f69ca5925af4d00592340f645a62c
alex <alex@pdp7.net>
2018-08-22 11:43:11 +0200

    Fix py_modules

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyduper',
-      packages=find_packages(),
+      py_modules=['pyduper'],
       extras_require={
         'dev': ['ipython', 'ipdb',],
       },
